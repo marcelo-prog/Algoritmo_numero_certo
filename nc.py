d935de72e6cba64cfd27abf25bee60cf85296081
MARCELO GOMES CASTANHO <71048436+marcelo-prog@users.noreply.github.com>
2021-02-10 21:31:13 -0300

    Create nc.py

---
+++ nc.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número e o programa te dirá se esse número esta entre 100 e 200"))
+
+if numero >= 100 and numero <= 200:
+    print("Está entre 100 e 200.")
+else:
+    print("O número não está entre 100 e 200.")
